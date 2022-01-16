d9c01f0707408a4eb1dda191621c3e6e454f9d34
hariananthan <hariananthan93@gmail.com>
2022-01-16 15:13:27 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import AppBar from "@mui/material/AppBar";
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import Logo from "./assets/Logo.png";
+import Home from "./components/Home";
+
+
+const StyledButton = styled(Button)(({ theme, color = '' }) => ({
+  color:'white',
+  ':hover': {
+    color: '#008CBA',
+  },
+}));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <AppBar position="static" style={{ background: '#2E3B55' }}>
+        <Toolbar>
+          <img src={Logo} className="icon" /> 
+          <Typography
+            variant="h6"          
+            component="div"
+          >
+          HARIANANTHAN          
+          </Typography>
+          <Box       
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >          
+          </Box>
+          <StyledButton variant="text" >Home</StyledButton>
+          <StyledButton variant="text" >Projects</StyledButton>
+          <StyledButton variant="text" >Portfolio</StyledButton>  
+        </Toolbar>
+      </AppBar>
+      <Home/>
     </div>
   );
 }
